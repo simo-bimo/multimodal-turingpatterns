8640bc8b7f0473dc0caf7d12356eff7a94f772c1
simo-bimo <tinkeretan@gmail.com>
2024-05-01 03:22:07 +1000

    Reverted temporary changes to grayscoot so params now normal

--- grayscott.py
+++ grayscott.py
@@ -33,14 +33,14 @@ class GrayScott(Model):
 		H = self.inhibitor
 		
 		return (self.feed*(np.ones((self.x_count, self.y_count))-A)\
-				- 0.5 * A * np.power(H, 2)\
+				- A * np.power(H, 2)\
 				+ self.Da*self.laplace(A))
 	
 	def deltaH(self):
 		A = self.activator
 		H = self.inhibitor
 		
-		return (0.5* A * np.power(H, 2)\
+		return (A * np.power(H, 2)\
 				- (self.feed + self.decay)*H\
 				+ self.Dh*self.laplace(H))
 	
