9a143d77d2d833a3b6678d785e0d3bdf27a08be0
simo-bimo <tinkeretan@gmail.com>
2024-04-30 23:18:56 +1000

    Continued fiddling. Tried a triple scale to see if errors where because of specific value of one, it is to do with the laplacian. I think i may need to create a bigger laplacian grid. Also tried applying the laplacian multiple times, but it didn't change anything, same issue as double scale. Only option would be to relatively slow down the rest of the sim, and calculate more frames in the interim, but that seems tedious and computationally expensive.

--- simulations/gs.py
+++ simulations/gs.py
@@ -5,16 +5,28 @@ import numpy as np
 # gs.set_activator(np.ones((gs.x_count, gs.y_count)))
 # gs.add_inhibitor(r=0.2, amount=1.0)
 # Model.to_file(gs, "data/gs_simple_scaled", frames=1000, steps_per_frame=20)
-
+# 
 # gs = GrayScott(dt=1.0, bottom_left=(-2,-2), top_right=(2,2), activator_diffusion=0.5, inhibitor_diffusion=0.25)
 # gs.set_activator(np.ones((gs.x_count, gs.y_count)))
 # gs.add_inhibitor(r=0.2, amount=1.0)
 # Model.to_file(gs, "data/gs_simple_scaled_half", frames=1000, steps_per_frame=20)
+# 
+# gs = GrayScott(dt=1.0, bottom_left=(-2,-2), top_right=(2,2), activator_diffusion=2.0, inhibitor_diffusion=1.0)
+# gs.set_activator(np.ones((gs.x_count, gs.y_count)))
+# gs.add_inhibitor(r=0.4, amount=1.0)
+# Model.to_file(gs, "data/gs_simple_scaled_double", frames=1000, steps_per_frame=20)
 
-gs = GrayScott(dt=1.0, bottom_left=(-2,-2), top_right=(2,2), activator_diffusion=1.0, inhibitor_diffusion=0.5)
+# gs = GrayScott(dt=1.0, bottom_left=(-2,-2), top_right=(2,2), activator_diffusion=3.0, inhibitor_diffusion=1.5)
+# gs.set_activator(np.ones((gs.x_count, gs.y_count)))
+# gs.add_inhibitor(r=0.2, amount=1.0)
+# Model.to_file(gs, "data/gs_simple_scaled_triple", frames=1000, steps_per_frame=20)
+
+# attempt to generate bigger scaling by just applying the laplacian multiple times
+gs = GrayScott(dt=1.0, bottom_left=(-2,-2), top_right=(2,2))
 lapl = gs.laplace
-# just make the laplacian apply itself twice... see if it works.
 gs.laplace = lambda x: lapl(lapl(x))
 gs.set_activator(np.ones((gs.x_count, gs.y_count)))
-gs.add_inhibitor(r=0.2, amount=1.0)
-Model.to_file(gs, "data/gs_simple_scaled_double", frames=1000, steps_per_frame=20)
+gs.add_inhibitor(r=0.4, amount=1.0)
+Model.to_file(gs, "data/gs_multi_laplace", frames=1000, steps_per_frame=20)
+
+Model.create_animation("grayscott/gs_multi_laplace", "data/gs_multi_laplace", "Inhibitor")
